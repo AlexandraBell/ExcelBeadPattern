cd59e3cd864c9aa97f4e5a39857ff3c5432ae7f0
Fangflier <alex_e_bell@yahoo.com>
2019-03-21 12:45:48 -0400

    ExcelBeadPattern - Initial Project set up

---
+++ ExcelBeadPattern/ExcelBeadPatternTest/BeadTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExcelBeadPatternTest
+{
+    [TestClass]
+    public class BeadTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
