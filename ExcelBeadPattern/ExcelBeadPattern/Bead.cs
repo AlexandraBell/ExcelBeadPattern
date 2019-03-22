d246688e6b05c3956512e4b424794fc457a18d93
Fangflier <alex_e_bell@yahoo.com>
2019-03-22 12:30:53 -0400

    ExcelBeadPattern - Skeleton Code and Test framework set up

--- ExcelBeadPattern/ExcelBeadPattern/Bead.cs
+++ ExcelBeadPattern/ExcelBeadPattern/Bead.cs
@@ -3,13 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using System.Drawing;
 
 namespace ExcelBeadPattern
 {
     class Bead
     {
+        //This will be where we process the command line arguements
         static void Main(string[] args)
         {
+            //objects needed
+            Image beadIm;
+
+            //Validate file path and return an error to the user if it doesn't exist.
+            inputValidation(args);
+
+            //Once validated, make a local copy to preserve original file
+            beadIm = Image.FromFile(args[0]);
+
+
+        }
+
+        //Currently leaving this as taking the whole array, may change as I solidify design.
+        /*
+        * inputValidation will take user input file and make sure
+        * 1) The file has a valid extension for an image
+        * 2) The file can be opened
+        */
+        //TODO - Fill out
+        private static void inputValidation(string[] args)
+        {
+
+            throw new NotImplementedException();
         }
     }
 }
--- ExcelBeadPattern/ExcelBeadPatternTest/BeadTest.cs
+++ ExcelBeadPattern/ExcelBeadPatternTest/BeadTest.cs
@@ -6,9 +6,55 @@ namespace ExcelBeadPatternTest
     [TestClass]
     public class BeadTest
     {
+
         [TestMethod]
-        public void TestMethod1()
+        /*
+        * Test the inputValidation method with gibberish input
+        */
+        //TODO - Fill out
+        public void inputValidationTestGibberishInput()
         {
+
+        }
+
+        [TestMethod]
+        /*
+        * Test the inputValidation method with no extension on the file
+        */
+        //TODO - Fill out
+        public void inputValidationTestNoExtension()
+        {
+
+        }
+
+        [TestMethod]
+        /*
+        * Test the inputValidation method with a file that doesn't exist
+        */
+        //TODO - Fill out
+        public void inputValidationTestFileDoesNotExist()
+        {
+
+        }
+
+        [TestMethod]
+        /*
+        * Test the inputValidation method with a file that does exist
+        */
+        //TODO - Fill out
+        public void inputValidationTestFileExists()
+        {
+
+        }
+
+        [TestMethod]
+        /*
+        * Test the inputValidation method with a file that can open
+        */
+        //TODO - Fill out
+        public void inputValidationTestFileOpens()
+        {
+
         }
     }
 }
