252b89b0290c882fddc4240d05a001198844b479
Fangflier <alex_e_bell@yahoo.com>
2019-03-22 13:51:22 -0400

    ExcelBeadPattern - Skeleton Code for Excel methods set up

--- ExcelBeadPattern/ExcelBeadPattern/Bead.cs
+++ ExcelBeadPattern/ExcelBeadPattern/Bead.cs
@@ -13,16 +13,23 @@ namespace ExcelBeadPattern
         //This will be where we process the command line arguements
         static void Main(string[] args)
         {
-            //objects needed
-            Image beadIm;
+            //I also wanted the user to be able to specify their grid size
 
             //Validate file path and return an error to the user if it doesn't exist.
-            inputValidation(args);
+            inputFileNameValidation(args);
 
+            //Create objects before they're needed
+            Image beadIm;
             //Once validated, make a local copy to preserve original file
             beadIm = Image.FromFile(args[0]);
 
+            //Next we are going to want to parse the image and place it into the excel file
+            imageParsing(beadIm);
 
+            //I can use this to set the color of the cell
+            //[RangeObject].Interior.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.Red);
+
+            Workbook final = excelPopulation();
         }
 
         //Currently leaving this as taking the whole array, may change as I solidify design.
@@ -32,10 +39,29 @@ namespace ExcelBeadPattern
         * 2) The file can be opened
         */
         //TODO - Fill out
-        private static void inputValidation(string[] args)
+        private static void inputFileNameValidation(string[] args)
+
         {
 
             throw new NotImplementedException();
         }
+
+        //This method is going to transform the image into the excel file
+        //I'm still unsure if I want to store the information in array/map/object and 
+        //then have another method input it into the excel doc.
+        private static void imageParsing(Image beadIm)
+        {
+            throw new NotImplementedException();
+        }
+
+        //This will fill out the excel file and return a workbook
+        private static Workbook excelPopulation()
+        {
+            Workbook returnPopWorkbook = null;
+            
+                                    
+            return returnPopWorkbook;
+        }
+
     }
 }
